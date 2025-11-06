645a178bd0eed7921bfa1d02190cdee38114793b
Harsh <harsh.sharma@maxworthsystems.com>
2025-11-06 15:45:03 +0530

    Initial Commit - Uploading raw project

---
+++ ECommerceAPI/App_Start/SwaggerConfig.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI;
+using Swashbuckle.Application;
+using System.Web;
+using System.Web.Http;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+namespace ECommerceAPI
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            GlobalConfiguration.Configuration
+                .EnableSwagger(c => c.SingleApiVersion("v1", "ECommerceAPI"))
+                .EnableSwaggerUi();
+        }
+    }
+}
